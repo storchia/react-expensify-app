3660caeb5a96d99e8753e2bcdef70070d36c23bd
Sergio A. Torchia <sergiotorchia@hotmail.com>
2021-04-04 13:09:55 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -12,16 +12,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas Bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Rent", amount: 19500 }));
-//store.dispatch(setTextFilter("water"));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
